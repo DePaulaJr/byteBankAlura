c4777e0a7a1ad4996673574583b875eaf0a80f8b
DePaulaJr <chicomzb@gmail.com>
2021-05-25 03:05:49 -0300

    44

--- Conta.js
+++ Conta.js
@@ -5,25 +5,44 @@ export class Conta{
         this._agencia = agencia;
     }
 
+    set cliente(novoValor){
+        if(novoValor instanceof Cliente){
+            this._cliente = novoValor;
+        }
+    }
+
+    get cliente(){
+        return this._cliente;
+    }
+
+    get saldo(){
+        return this._saldo;
+    }
     sacar(valor) {
-        taxa = 1.1 * valor;
-        if (this._saldo >= valor) {
-            this._saldo -= valor;
+
+        let taxa = 1;
+        if(this._tipo == "corrente"){
+            taxa = 1.1;
+        }
+        if(this._tipo == "salario"){
+            taxa = 1.05;
+        }
+        if(this._tipo == "empresarial"){
+            taxa = 1.15;
+        }
+        const valorSacado = taxa * valor;
+        if (this._saldo >= valorSacado) {
+            this._saldo -= valorSacado;
             return valor;
         }
     }
 
     depositar(valor) {
-        if (valor <= 100) {
-            return;
-        }
         this._saldo += valor;
     }
 
     tranferir(valor, conta) {
-
         const valorSacado = this.sacar(valor);
         conta.depositar(valorSacado);
-
     }
 }
